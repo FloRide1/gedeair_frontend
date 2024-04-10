e240ce9be11b89cc74031fb139bf06a00dfbb7a9
FloRide1 <florian.reimat@orange.fr>
2024-04-10 22:53:13 +0200

    feat(login): Add redirection on Login button to backend login endpoint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,32 @@
-<script setup lang="ts"></script>
-
 <template>
     <main>
         <div
             class="position-absolute d-flex align-center justify-center w-100 h-100"
         >
-            <v-btn variant="tonal" size="x-large"> Login </v-btn>
+            <v-btn
+                :loading="loading"
+                :disabled="loading"
+                variant="tonal"
+                size="x-large"
+                href="/api/login"
+            >
+                Login
+            </v-btn>
         </div>
     </main>
 </template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            loading: false,
+        };
+    },
+    methods: {
+        clientLogin() {
+            this.loading = true;
+        },
+    },
+};
+</script>
