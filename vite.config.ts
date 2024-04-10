b1091504f07fcedec3ca7a81604be5a86b88555b
FloRide1 <florian.reimat@orange.fr>
2024-04-10 22:52:39 +0200

    feat(vite): Load dotenv on startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dotenv from "dotenv";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 import VueDevTools from "vite-plugin-vue-devtools";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
