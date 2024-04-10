22e17a47f3da7968814ad4688d23fc00d4b1c41b
FloRide1 <florian.reimat@orange.fr>
2024-04-10 22:18:02 +0200

    feat(home): Add centered Login Button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-    <main></main>
+    <main>
+        <div
+            class="position-absolute d-flex align-center justify-center w-100 h-100"
+        >
+            <v-btn variant="tonal" size="x-large"> Login </v-btn>
+        </div>
+    </main>
 </template>
