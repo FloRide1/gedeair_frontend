df0e151e05d4206d1f5f153871ddb476835c7714
FloRide1 <florian.reimat@orange.fr>
2024-04-14 12:27:51 +0200

    fix(vite): Readd changeOrigin + cookieDomainChange

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import VueDevTools from "vite-plugin-vue-devtools";
 
 dotenv.config();
 
+const backend_url = process.env.BACKEND_BASE_URL || import.meta.url + "/api";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), VueDevTools()],
@@ -18,9 +20,10 @@ export default defineConfig({
     server: {
         proxy: {
             "^/api/.*": {
-                target:
-                    process.env.BACKEND_BASE_URL || import.meta.url + "/api",
+                target: backend_url,
                 rewrite: (path) => path.replace(/^\/api/, ""),
+                changeOrigin: true,
+                cookieDomainRewrite: backend_url,
             },
         },
     },
